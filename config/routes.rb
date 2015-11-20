16673629a88b6c98398a8fa0f232033b346f609c
pepeuriel <j.urielsz@gmail.com>
2015-11-20 17:15:43 -0600

    Create project and add devise

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'cart/index'
+
+  resources :products
+  root 'page#home'
+
+  get 'page/about'
+
+  get 'page/faqs'
+
+  get 'page/contact'
+  
+end
